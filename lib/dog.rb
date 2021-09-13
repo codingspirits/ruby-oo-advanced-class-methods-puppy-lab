f9468637ec7cbd5411e3463967bc7fdec5a45680
vincci-cheng-project-development-page <vincci.cheng.github@gmail.com>
2021-09-13 20:12:35 +1000

    Completed Assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,34 @@
-# Add your code here
+class Dog
+    @@all = []
+    attr_accessor :name
+    def initialize(name)
+        @name = name
+        #@@all.push(self)
+        save
+    end 
+    def self.all
+        @@all
+    end
+    def self.clear_all
+        @@all.length.times do @@all.shift()
+            @@all
+        end
+    end
+    def self.print_all
+        @@all.each do |id| 
+            puts id.name
+        end
+    end
+    def save 
+        @@all.push(self)
+    end
+end
+
+dog1 = Dog.new("scooby")
+dog2 = Dog.new("snoopy")
+p Dog.all
+Dog.print_all
+Dog.clear_all
+p Dog.all
+
+
